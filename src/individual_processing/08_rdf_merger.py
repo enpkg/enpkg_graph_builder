39be3e8cd3656b0a48b40854e562a3e510cd82ed
Arnaud Gaudry <gaudry.arnaud@gmail.com>
2023-05-18 09:51:47 +0200

    typo in list of files

--- src/individual_processing/08_rdf_merger.py
+++ src/individual_processing/08_rdf_merger.py
@@ -44,7 +44,7 @@ df_list = []
 files = ["rdf/canopus_pos.ttl", "rdf/canopus_neg.ttl", 
         "rdf/features_pos.ttl", "rdf/features_neg.ttl", 
         "rdf/features_spec2vec_pos.ttl", "rdf/features_spec2vec_neg.ttl",
-        "rdf/indivual_mn_pos.ttl", "rdf/indivual_mn_neg.ttl",
+        "rdf/individual_mn_pos.ttl", "rdf/individual_mn_neg.ttl",
         "rdf/isdb_pos.ttl", "rdf/isdb_neg.ttl",
         "rdf/sirius_pos.ttl", "rdf/sirius_neg.ttl",
         "rdf/metadata_enpkg.ttl", "rdf/metadata_module_enpkg.ttl",
